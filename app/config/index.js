38a59ac7851b381ebda8fcc77ca8b6ee4642b963
unknown <397694525@qq.com>
2018-10-08 09:48:41 +0800

    更新app中的vue组件

--- app/config/index.js
+++ app/config/index.js
@@ -4,7 +4,7 @@
 
 const path = require('path')
 
-module.exports = {
+module.expornpm config list -lts = {
   dev: {
 
     // Paths
--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html',require('ejs'.__express));
+app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
 
 app.use(logger('dev'));
